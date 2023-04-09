46dea854b9544a5bcb193aa7ad420be1dd9b5497
thotran <thotran.exe@gmail.com>
2023-04-09 22:26:38 +0000

    please work

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # 482-104-project
-I followed the tutorial on how to setup a dev container in order to create a remote container, the installation process of docker, and watch the video tutorial on the docker basics. Still unsure on how to set up the container myself without the extension and earlier i was able to create my own docker image that when ran printed hello world. I didnt know exactly what kind of screen shoot to attach so i attached the terminal in vscode and the docker desktop terminal 
-### Vscode terminal running main.py
-![Alt text](proof.png)
-### Docker Desktop terminal running main.py
-![Alt text](proof2.png)
+[HF SPACE LINK HERE](https://huggingface.co/spaces/thotranexe/milestone2)
+I followed the tutorial posted and created a [seperate repo](https://github.com/thotranexe/milestone2) for this milestone however all the code is stil was written and copy pasted on this branch just in case. I understand that I probably shouldve just used this branch as the repo to deploy the HF spaces from. In the future I will use this repo to deply from. 
+
+The model I used was the [twitter-roberta-base-sentiment-model](https://huggingface.co/cardiffnlp/twitter-xlm-roberta-base-sentiment?text=T%27estimo%21) and i just tweaked it so that it would print on streamlits web app instead.
--- main.py
+++ main.py
@@ -1,1 +1,44 @@
-print("stinky")
+from transformers import AutoModelForSequenceClassification
+from transformers import TFAutoModelForSequenceClassification
+from transformers import AutoTokenizer, AutoConfig
+import numpy as np
+from scipy.special import softmax
+import streamlit as st
+st.title("Tho Tran - Milestone2")
+# Preprocess text (username and link placeholders)
+def preprocess(text):
+    new_text = []
+    for t in text.split(" "):
+        t = '@user' if t.startswith('@') and len(t) > 1 else t
+        t = 'http' if t.startswith('http') else t
+        new_text.append(t)
+    return " ".join(new_text)
+
+MODEL = f"cardiffnlp/twitter-xlm-roberta-base-sentiment"
+
+tokenizer = AutoTokenizer.from_pretrained(MODEL)
+config = AutoConfig.from_pretrained(MODEL)
+
+# PT
+model = AutoModelForSequenceClassification.from_pretrained(MODEL)
+model.save_pretrained(MODEL)
+tokenizer.save_pretrained(MODEL)
+
+text = st.text_input("Enter text here:","I love you")
+text = preprocess(text)
+encoded_input = tokenizer(text, return_tensors='pt')
+output = model(**encoded_input)
+scores = output[0][0].detach().numpy()
+scores = softmax(scores)
+
+
+# Print labels and scores
+ranking = np.argsort(scores)
+ranking = ranking[::-1]
+for i in range(scores.shape[0]):
+    l = config.id2label[ranking[i]]
+    s = scores[ranking[i]]
+    # print(f"{i+1}) {l} {np.round(float(s), 4)}")
+    st.write(l,np.round(float(s),4))
+
+st.write("choosen model is https://huggingface.co/cardiffnlp/twitter-xlm-roberta-base-sentiment?text=T%27estimo%21")
